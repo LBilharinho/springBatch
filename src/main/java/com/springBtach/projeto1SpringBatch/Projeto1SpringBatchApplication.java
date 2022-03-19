ffa6cb8bdc0a3f7af87afde52c07e7cca808acc0
Lucas Bilharinho <49289812+LBilharinho@users.noreply.github.com>
2022-03-19 16:12:19 -0300

    First commit

---
+++ src/main/java/com/springBtach/projeto1SpringBatch/Projeto1SpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.springBtach.projeto1SpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto1SpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto1SpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBtach/projeto1SpringBatch/Projeto1SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBtach.projeto1SpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto1SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
