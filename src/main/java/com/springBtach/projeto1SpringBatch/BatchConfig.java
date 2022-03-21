1fff63adbc1c898d2a0a0b338c7412bd1de4b146
Lucas Bilharinho <49289812+LBilharinho@users.noreply.github.com>
2022-03-21 16:47:32 -0300

    implementacao read | processor | writer

--- src/main/java/com/springBtach/projeto1SpringBatch/BatchConfig.java
+++ src/main/java/com/springBtach/projeto1SpringBatch/BatchConfig.java
@@ -6,14 +6,17 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableBatchProcessing
+/*@EnableBatchProcessing
 @Configuration
 public class BatchConfig {
 	@Autowired
@@ -26,7 +29,7 @@ public class BatchConfig {
 		return jobBuilderFactory
 				.get("imprimeHelloJob")
 				.start(imprimeHelloStep())
-				//.incrementer(new RunIdIncrementer())
+				.incrementer(new RunIdIncrementer())
 				.build();
 
 	}
@@ -35,15 +38,22 @@ public class BatchConfig {
 	public Step imprimeHelloStep() {
 		return stepBuildFactory
 				.get("imprimeHelloStep")
-				.tasklet(new Tasklet() {
-
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("Hello World");
-				return RepeatStatus.FINISHED;
-			}
-		}).build();
+				.tasklet(imprimeHelloTasklet(null))
+				.build();
 
 	}
 
+	@Bean
+	@StepScope
+	public Tasklet imprimeHelloTasklet(@Value("#{jobParameters['nome']}")String nome) {
+		return new Tasklet() {
+
+@Override
+public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		System.out.println("Hello World");
+		return RepeatStatus.FINISHED;
 }
+};
+	}
+
+}*/
--- src/main/java/com/springBtach/projeto1SpringBatch/Projeto1SpringBatchApplication.java
+++ src/main/java/com/springBtach/projeto1SpringBatch/Projeto1SpringBatchApplication.java
@@ -3,11 +3,11 @@ package com.springBtach.projeto1SpringBatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/*@SpringBootApplication
 public class Projeto1SpringBatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto1SpringBatchApplication.class, args);
 	}
 
-}
+}*/
